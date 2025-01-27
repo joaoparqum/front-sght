823e2722d4753cc76e0f14dbc6af0626d7408975
João Paulo <costajoaopaulo113@gmail.com>
2025-01-27 10:46:03 -0300

    feat: Dockerfile

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,16 +7,17 @@ import HomeScreen from '../components/HomeScreen.vue';
 import TelaEditaSolicitacao from '../components/TelaEditaSolicitacao.vue';
 import TelaHorasValidas from '../components/TelaHorasValidas.vue';
 import TelaCriaHoraValida from '../components/TelaCriaHoraValida.vue';
+import { useStore } from 'vuex';
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/HomeScreen', component: HomeScreen},
-    { path: '/CriaUsuario', component: TelaCriaUsuario},
-    { path: '/CriaSolicitacao', component: TelaCriaSolicitacao},
-    { path: '/CriaHoraValida', component: TelaCriaHoraValida},
-    { path: '/TelaSolicitacao', component: TelaSolicitacoes},
-    { path: '/EditaSolicitacao', component: TelaEditaSolicitacao},
-    { path: '/TelaHorasValidas', component: TelaHorasValidas}
+    { path: '/HomeScreen', component: HomeScreen, meta: { requiresAuth: true }},
+    { path: '/CriaUsuario', component: TelaCriaUsuario, meta: { requiresAuth: true }},
+    { path: '/CriaSolicitacao', component: TelaCriaSolicitacao, meta: { requiresAuth: true }},
+    { path: '/CriaHoraValida', component: TelaCriaHoraValida, meta: { requiresAuth: true }},
+    { path: '/TelaSolicitacao', component: TelaSolicitacoes, meta: { requiresAuth: true }},
+    { path: '/EditaSolicitacao', component: TelaEditaSolicitacao, meta: { requiresAuth: true }},
+    { path: '/TelaHorasValidas', component: TelaHorasValidas, meta: { requiresAuth: true }}
 ];
   
 const router = createRouter({
@@ -25,7 +26,7 @@ const router = createRouter({
 });
 
 // Adicionando a guarda de navegação para proteger as rotas
-/*router.beforeEach((to, _from, next) => {
+router.beforeEach((to, _from, next) => {
   const store = useStore();
 
   // Checa se a rota requer autenticação
@@ -39,6 +40,6 @@ const router = createRouter({
   } else {
     next(); // Permite o acesso para rotas que não precisam de autenticação
   }
-});*/
+});
 
 export default router;
